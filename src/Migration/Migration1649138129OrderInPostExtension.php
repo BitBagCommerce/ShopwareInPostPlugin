adf59829637a9947e2490d75dd3e08939266eafa
Norbert Glanc <norglanc@gmail.com>
2022-04-05 11:34:29 +0200

    Add extension for save point name and package id

---
+++ src/Migration/Migration1649138129OrderInPostExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\ShopwareInPostPlugin\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1649138129OrderInPostExtension extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1649138129;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $sql = <<<SQL
+CREATE TABLE IF NOT EXISTS `order_inpost_extension` (
+    `id` BINARY(16) NOT NULL,
+    `point_name` VARCHAR(20) NOT NULL,
+    `package_id` INT(11) NULL,
+    `order_id` BINARY(16) NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+SQL;
+
+        $connection->executeStatement($sql);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+    }
+}
