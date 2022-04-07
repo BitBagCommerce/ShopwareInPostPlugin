4e0c879aa21001a435e2f072e2f46b2d7ee97376
Norbert Glanc <norglanc@gmail.com>
2022-04-07 07:24:52 +0200

    Create const for inPost property

---
+++ src/Extension/Content/Order/OrderInPostExtensionInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\ShopwareInPostPlugin\Extension\Content\Order;
+
+interface OrderInPostExtensionInterface
+{
+    public const PROPERTY_KEY = 'inPost';
+}
