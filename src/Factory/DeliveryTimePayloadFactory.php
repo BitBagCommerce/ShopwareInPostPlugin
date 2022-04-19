88d1104666e525228ad11a66bc6ee0153117e3de
Norbert Glanc <norglanc@gmail.com>
2022-04-19 11:13:18 +0200

    Remove id and createdAt property from factories

--- src/Factory/DeliveryTimePayloadFactory.php
+++ src/Factory/DeliveryTimePayloadFactory.php
@@ -4,19 +4,15 @@ declare(strict_types=1);
 
 namespace BitBag\ShopwareInPostPlugin\Factory;
 
-use Shopware\Core\Framework\Uuid\Uuid;
-
 final class DeliveryTimePayloadFactory implements DeliveryTimePayloadFactoryInterface
 {
     public function create(): array
     {
         return [
-            'id' => Uuid::randomHex(),
             'name' => '1-3 days',
             'min' => 1,
             'max' => 3,
             'unit' => 'day',
-            'createdAt' => new \DateTime(),
         ];
     }
 }
