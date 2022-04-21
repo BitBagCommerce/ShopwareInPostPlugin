589d2f6a04739c69895d7158ceff818d852a1f82
Norbert Glanc <norglanc@gmail.com>
2022-04-21 12:41:43 +0200

    Remove not used const and functions in WebClient && WebClientInterface

--- src/Api/WebClientInterface.php
+++ src/Api/WebClientInterface.php
@@ -16,31 +16,7 @@ interface WebClientInterface
 
     public const API_VERSION = 'v1';
 
-    public const CONFIRMED_STATUS = 'confirmed';
-
     public const INPOST_LOCKER_STANDARD_SERVICE = 'inpost_locker_standard';
-
-    public const INPOST_LOCKER_PASS_THRU_SERVICE = 'inpost_locker_pass_thru';
-
-    public const SMS_ADDITIONAL_SERVICE = 'sms';
-
-    public const EMAIL_ADDITIONAL_SERVICE = 'email';
-
-    public const SATURDAY_ADDITIONAL_SERVICE = 'saturday';
-
-    public const ROD_ADDITIONAL_SERVICE = 'rod';
-
-    public const STATUS_CREATED = 'created';
-
-    public function getPointByName(string $name, int $attempts = 0): ?array;
-
-    public function getOrganizations(): array;
-
-    public function getShipmentById(int $id): ?array;
-
-    public function getLabels(array $shipmentIds): ?string;
-
-    public function getShipments(): ?array;
 
     public function createShipment(array $data): array;
 
