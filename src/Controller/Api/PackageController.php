e51b60567fd81e4c8af2033ba2391d43507e54d3
Norbert Glanc <norglanc@gmail.com>
2022-04-28 17:26:25 +0200

    Change define route scope in controllers

--- src/Controller/Api/PackageController.php
+++ src/Controller/Api/PackageController.php
@@ -11,12 +11,13 @@ use BitBag\ShopwareInPostPlugin\Resolver\OrderExtensionDataResolverInterface;
 use OpenApi\Annotations as OA;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route(defaults={"_routeScope"={"api"}})
+ * @RouteScope(scopes={"api"})
  */
 final class PackageController
 {
