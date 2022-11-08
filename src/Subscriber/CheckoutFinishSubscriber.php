fd9ff7dd38fa0eee519594297c2c5eb1607bfd2f
Gabriela Lubkowska <dsw45017@student.dsw.edu.pl>
2022-11-08 12:09:00 +0100

    Add inPost point details to order confirmation

---
+++ src/Subscriber/CheckoutFinishSubscriber.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start.
+ * You can find more information about us on https://bitbag.io and write us an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\ShopwareInPostPlugin\Subscriber;
+
+use BitBag\ShopwareInPostPlugin\Factory\ShippingMethodPayloadFactoryInterface;
+use GuzzleHttp\Client;
+use Shopware\Core\Framework\Struct\ArrayStruct;
+use Shopware\Storefront\Page\Checkout\Finish\CheckoutFinishPageLoadedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+
+final class CheckoutFinishSubscriber implements EventSubscriberInterface
+{
+    private Client $client;
+
+    public function __construct(Client $client )
+    {
+        $this->client = $client;
+    }
+
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            CheckoutFinishPageLoadedEvent::class => 'checkoutFinishPageLoaded'
+        ];
+    }
+
+    public function checkoutFinishPageLoaded(CheckoutFinishPageLoadedEvent $event): void
+    {
+        $shippingMethod = $event->getSalesChannelContext()->getShippingMethod()->getName();
+
+        if ($shippingMethod == ShippingMethodPayloadFactoryInterface::SHIPPING_KEY)
+        {
+            $pointDetails = $this->fetchPoint($event);
+
+            $event->getPage()->addExtension('point_details', new ArrayStruct($pointDetails));
+        }
+    }
+
+    public function fetchPoint(CheckoutFinishPageLoadedEvent $event): array
+    {
+        $point = $event->getPage()->getOrder()->getExtensions()['inPost']['pointName'];
+        $url = 'https://api-pl-points.easypack24.net/v1/points/'.$point;
+
+        $pointDetailsData = $this->client->request(
+            'GET',
+            $url
+        )->getBody()->getContents();
+
+        $pointDetails = json_decode($pointDetailsData, true);
+
+        return $pointDetails;
+    }
+}
