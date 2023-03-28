15d9530c5266ef4c68a74492f05f51a0408991b7
milwoz <milwoz95@gmail.com>
2023-03-28 07:52:39 +0200

    fix: ecs check

--- src/Finder/CashOnDeliveryPaymentMethodFinder.php
+++ src/Finder/CashOnDeliveryPaymentMethodFinder.php
@@ -11,8 +11,8 @@ declare(strict_types=1);
 namespace BitBag\ShopwareInPostPlugin\Finder;
 
 use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\CashPayment;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\IdSearchResult;
